1328be8fd226040e81c3e4ef6bca0ec623ec68b0
komplonk456 <61110325+komplonk456@users.noreply.github.com>
2020-02-16 18:04:24 +0700

    Update tembak.php

--- tembak.php
+++ tembak.php
@@ -61,7 +61,7 @@ echo " ===========================\n";
         echo color("yellow",".");
         sleep(1);
         }
-        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"GOFOODSANTAI19"}');
+        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"GOFOOD021120A"}');
         $message = fetch_value($code1,'"message":"','"');
         if(strpos($code1, 'Promo kamu sudah bisa dipakai')){
         echo "\n".color("green","+] Message: ".$message);
