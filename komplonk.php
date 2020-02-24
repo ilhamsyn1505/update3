1313f6fdc31c6edd5ac1572775170c59bb8eb3c9
komplonk456 <61110325+komplonk456@users.noreply.github.com>
2020-02-24 21:28:27 +0700

    Update komplonk.php

--- komplonk.php
+++ komplonk.php
@@ -25,7 +25,8 @@ function change(){
         echo color("green","+] Berhasil mendaftar");
         $token = getStr('"access_token":"','"',$verif);
         $uuid = getStr('"resource_owner_id":',',',$verif);
-        echo "\n".color("nevy","?] Mau Redeem Voucher?: y/n ");"n";"\n";
+        echo "\n".color("nevy","?] Mau Redeem Voucher?: y/n ");
+        echo "\n"."n"."\n";
         $pilihan = (fgets(STDIN));
         if($pilihan == "y" || $pilihan == "Y"){
         echo color("red","===========(REDEEM VOUCHER)===========");
@@ -98,7 +99,8 @@ function change(){
         $expired3 = getStr1('"expiry_date":"','"',$cekvoucher,'3');
         $expired4 = getStr1('"expiry_date":"','"',$cekvoucher,'4');
          setpin:
-         echo "\n".color("nevy","?] Mau set pin?: y/n ");"y";"\n";
+         echo "\n".color("nevy","?] Mau set pin?: y/n ");
+         echo "\n"."n"."\n";
          $pilih1 = trim(fgets(STDIN));
          if($pilih1 == "y" || $pilih1 == "Y"){
          //if($pilih1 == "y" && strpos($no, "628")){
