f36c945dcec56d89fc45934881197e8bce9f8a82
komplonk456 <61110325+komplonk456@users.noreply.github.com>
2020-02-27 18:42:54 +0700

    Update visop.php

--- visop.php
+++ visop.php
@@ -163,7 +163,6 @@ function change(){
          echo color("red","========( PIN ANDA = 112233 )========")."\n";
          $data2 = '{"pin":"112233"}';
          $getotpsetpin = request("/wallet/pin", $token, $data2, null, null, $uuid);
-         otpsetpin:
          echo "Otp set pin: ";
          $otpsetpin = trim(fgets(STDIN));
          $verifotpsetpin = request("/wallet/pin", $token, $data2, null, $otpsetpin, $uuid);
@@ -171,15 +170,11 @@ function change(){
          }else if($pilih1 == "n" || $pilih1 == "N"){
          die();
          }else{
-         echo color("red","-] Otp yang anda input salah");
-         echo"\n==================================\n\n";
-         echo color("yellow","!] Silahkan input kembali\n");
-         goto otpsetpin;
+         echo color("red","-] GAGAL!!!\n");
+         } 
          }
          }
          }
-         }
-         }else{
          goto setpin;
          }
          }else{
