e9e90def9e68a49f3127c649e2c13b5580ad32f0
komplonk456 <61110325+komplonk456@users.noreply.github.com>
2020-02-29 02:03:49 +0700

    Update visop1.php

--- visop1.php
+++ visop1.php
@@ -170,20 +170,19 @@ function change(){
          $messageverifotpsetpin = fetch_value($verifotpsetpin,'"message":"','"');
          if(strpos($verifotpsetpin, 'OTP kamu tidak berlaku. Silakan masukkan OTP yang masih berlaku.')){
          echo "\n".color("red","-] Message: ".$messageverifotpsetpin);{
-         goto ;
+         
+         
+         }else{
+         echo "\n".color("green","+] Message: ".$messageverifotpsetpin);
+         if($pilih1 == "n" || $pilih1 == "N"){
+         die();
          echo "\n".color("nevy"," Mau ulang? (y/n): ");
-         $pilih7 = trim(fgets(STDIN));
-         echo "\n".color("yellow","!] (Y/y): Kirim Ulang SMS Otp");
-         echo "\n".color("yellow","!] (N/n): Jika Salah Ketik Otp");
+         }else{
          if($pilih7 == "y" || $pilih == "Y"){
          goto setpin;
          }else{
          if($pilih7 == "n" || $pilih == "N"){
          goto otpsetpin;
-         }else{
-         echo "\n".color("green","+] Message: ".$messageverifotpsetpin);
-         if($pilih1 == "n" || $pilih1 == "N"){
-         die();
          }else{
          echo color("green","+] SUKSES!!!\n");
          }
