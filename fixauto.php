ca05748ca8ee80450dbee90257511c1c9fd1e746
komplonk456 <61110325+komplonk456@users.noreply.github.com>
2020-03-02 14:51:01 +0700

    Update fixauto.php

--- fixauto.php
+++ fixauto.php
@@ -160,6 +160,7 @@ function change(){
          }else{
          goto setpin;
          }
+         }
          }else{
          echo color("red","-] Otp yang anda input salah");
          echo"\n==================================\n\n";
