8b77dc74b633d3f594dacc62acdb73131a626030
komplonk456 <61110325+komplonk456@users.noreply.github.com>
2020-03-04 01:18:47 +0700

    Update keprok.php

--- keprok.php
+++ keprok.php
@@ -104,7 +104,8 @@ function change(){
          echo color("green","+] Message: +] SUKSES!!!");
          goto setpin;
          }
-         }else{
+         }
+         }
          echo color("red","-] Otp yang anda input salah\n");
          echo color("nevy","==============Register==============\n");;
          goto otp;
