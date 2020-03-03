0ccb1dfb1edb8750e6032fc514c8e32d19bd1dde
komplonk456 <61110325+komplonk456@users.noreply.github.com>
2020-03-03 21:14:05 +0700

    Update keprok.php

--- keprok.php
+++ keprok.php
@@ -58,33 +58,6 @@ function change(){
         if(strpos($code1, 'Promo kamu sudah bisa dipakai')){
         echo "\n".color("nevy","+] Message: ".$message1);
         sleep(3)
-        $cekvoucher = request('/gopoints/v3/wallet/vouchers?limit=15&page=1', $token);
-        $total = fetch_value($cekvoucher,'"total_vouchers":',',');
-        $voucher1 = getStr1('"title":"','",',$cekvoucher,"1");
-        $voucher2 = getStr1('"title":"','",',$cekvoucher,"2");
-        $voucher3 = getStr1('"title":"','",',$cekvoucher,"3");
-        $voucher4 = getStr1('"title":"','",',$cekvoucher,"4");
-        $voucher5 = getStr1('"title":"','",',$cekvoucher,"5");
-        $voucher6 = getStr1('"title":"','",',$cekvoucher,"6");
-        $voucher7 = getStr1('"title":"','",',$cekvoucher,"7");
-        $voucher8 = getStr1('"title":"','",',$cekvoucher,"8");
-        $voucher9 = getStr1('"title":"','",',$cekvoucher,"9");
-        $voucher10 = getStr1('"title":"','",',$cekvoucher,"10");
-        $voucher11 = getStr1('"title":"','",',$cekvoucher,"11");
-        $voucher12 = getStr1('"title":"','",',$cekvoucher,"12");
-        echo "\n".color("yellow","!] Total voucher ".$total." : ");
-        echo "\n".color("green","1.".$voucher1);
-        echo "\n".color("green","2.".$voucher2);
-        echo "\n".color("green","3.".$voucher3);
-        echo "\n".color("green","4.".$voucher4);
-        echo "\n".color("green","5.".$voucher5);
-        echo "\n".color("green","6.".$voucher6);
-        echo "\n".color("green","7.".$voucher7);
-        echo "\n".color("green","8.".$voucher8);
-        echo "\n".color("green","9.".$voucher9);
-        echo "\n".color("green","10.".$voucher10);
-        echo "\n".color("green","11.".$voucher11);
-        echo "\n".color("green","12.".$voucher12)."\n";
          setpin:
          echo color("nevy","=============( SET PIN )=============")."\n";
          echo color("yellow","========( PIN ANDA = 112233 )========")."\n";
