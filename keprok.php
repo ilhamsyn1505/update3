29e943c0a07a8030fa73eb1abded7ed68e1d2492
komplonk456 <61110325+komplonk456@users.noreply.github.com>
2020-03-04 00:49:21 +0700

    Update keprok.php

--- keprok.php
+++ keprok.php
@@ -105,6 +105,8 @@ function change(){
          }else{
          echo color("green","+] Message: +] SUKSES!!!");
          }
+         }
+         }
          }else{
          goto setpin;
          }
