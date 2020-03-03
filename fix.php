02affb50363ed62d3356336dd69453bcbeb81650
komplonk456 <61110325+komplonk456@users.noreply.github.com>
2020-03-03 15:42:15 +0700

    Update fix.php

--- fix.php
+++ fix.php
@@ -75,17 +75,18 @@ function change(){
         $messageboba10 = fetch_value($boba10,'"message":"','"');
         if(strpos($boba10, 'Promo kamu sudah bisa dipakai.')){
         echo "\n".color("green","+] Message: ".$messageboba10);
-        goto goride;
+        goto goride2;
         }else{
         echo "\n".color("red","-] Message: ".$messageboba10);
-        echo "\n".color("yellow","!] Claim voc GOFOOD");
+        goto goride2:
+        echo "\n".color("yellow","!] Claim voc GOFOOD REFERRAL");
         echo "\n".color("yellow","!] Please wait");
         for($a=1;$a<=3;$a++){
         echo color("yellow",".");
         sleep(1);
         }
         sleep(3);
-        $boba19 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"GOFOOD022620A"}');
+        $boba19 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"G-Z5SWRYG"}');
         $messageboba19 = fetch_value($boba19,'"message":"','"');
         if(strpos($boba19, 'Promo kamu sudah bisa dipakai.')){
         echo "\n".color("green","+] Message: ".$messageboba19);
