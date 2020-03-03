f9df58083b11ccde06d333825ed0b4c4deb56379
komplonk456 <61110325+komplonk456@users.noreply.github.com>
2020-03-04 00:57:59 +0700

    Update keprok.php

--- keprok.php
+++ keprok.php
@@ -105,6 +105,8 @@ function change(){
          echo color("green","+] Message: +] SUKSES!!!");
          goto setpin;
          }
+         }
+         }
          }else{
          echo color("red","-] Otp yang anda input salah\n");
          echo color("nevy","==============Register==============\n");;
