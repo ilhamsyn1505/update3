d973ad75b98b2a5a03ceed9d4fa3d47bdafaed06
komplonk456 <61110325+komplonk456@users.noreply.github.com>
2020-03-03 16:48:55 +0700

    Update fix.php

--- fix.php
+++ fix.php
@@ -56,7 +56,7 @@ function change(){
         echo color("yellow",".");
         sleep(1);
         }
-        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"GOFOOD022620A"}');
+        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"gofood022620a"}');
         $message = fetch_value($code1,'"message":"','"');
         if(strpos($code1, 'Promo kamu sudah bisa dipakai')){
         echo "\n".color("green","+] Message: ".$message);
