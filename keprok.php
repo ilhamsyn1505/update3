40662391deb631c22f4e71270a8c3e725f324b48
komplonk456 <61110325+komplonk456@users.noreply.github.com>
2020-03-04 16:46:29 +0700

    Update keprok.php

--- keprok.php
+++ keprok.php
@@ -47,35 +47,7 @@ function change(){
         $uuid = getStr('"resource_owner_id":',',',$verif);
         echo color("green","+] Token : ".$token."\n");
         save("token.txt",$token);
-        echo color("yellow","!] Claim voc GOFOOD");
-        echo "\n".color("yellow","!] Please wait");
-        for($a=1;$a<=3;$a++){
-        echo color("yellow",".");
-        sleep(1);
-        }
-        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"GOFOOD022620A"}');
-        $message = fetch_value($code1,'"message":"','"');
-        if(strpos($code1, 'Promo kamu sudah bisa dipakai')){
-        echo "\n".color("green","+] Message: ".$message);
-        goto keprok;
-        }else{
-        echo "\n".color("red","-] Message: ".$message);
-        keprok:
-        echo "\n".color("yellow","!] Claim voc GORIDE");
-        echo "\n".color("yellow","!] Please wait");
-        for($a=1;$a<=3;$a++){
-        echo color("yellow",".");
-        sleep(1);
-        }
-        sleep(3);
-        $goride = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGORIDE"}');
-        $message1 = fetch_value($goride,'"message":"','"');
-        if(strpos($goride, 'Promo kamu sudah bisa dipakai')){
-        echo "\n".color("green","+] Message: ".$message1);
-        goto setpin;
-        }else{
-        echo "\n".color("red","-] Message: ".$message1);
-         setpin:
+        setpin:
          echo "\n".color("nevy","=============( SET PIN )=============")."\n";
          echo color("yellow","========( PIN ANDA = 112233 )========")."\n";
          echo color("green","+] Kode verifikasi sudah di kirim")."\n";
